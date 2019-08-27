3b377199be504a258276ce54d07ba8c4fe3dda0e
gabir-yusuf <gabir.yusuf@gmail.com>
2019-08-27 21:50:20 +0200

    add quiz 2.1 solution

--- 2_SQL Joins/Quiz_2.sql
+++ 2_SQL Joins/Quiz_2.sql
@@ -2,6 +2,15 @@
 -- @Date: 26 of Aug 2019
 ---------------------------------------------------------------------------------
 /*
-Quiz JION.1 Description:
-	Try pulling all the data from the accounts table, and all the data from the orders table.
+Quiz 2.1 Description:
+	Provide a table for all web_events associated with account name of Walmart. 	There should be three columns. Be sure to include the primary_poc, time of 		the event, and the channel for each event. Additionally, you might choose to 	add a fourth column to assure only Walmart events were chosen. 
 */
+SELECT a.primary_poc, w.occurred_at, w.channel, a.name
+FROM web_events w
+JOIN accounts a
+ON w.account_id = a.id
+WHERE a.name = 'Walmart';
+
+--------------------------------------------------------------------------------
+
+
