05561d79a78e44a7d450368afb3d36f85492692f
gabir-yusuf <gabir.yusuf@gmail.com>
2019-08-27 21:41:53 +0200

    add Quiz 2

---
+++ 2_SQL Joins/Quiz_2.sql
@@ -0,0 +1,7 @@
+-- @auther: Gabir N. Yousef
+-- @Date: 26 of Aug 2019
+---------------------------------------------------------------------------------
+/*
+Quiz JION.1 Description:
+	Try pulling all the data from the accounts table, and all the data from the orders table.
+*/
