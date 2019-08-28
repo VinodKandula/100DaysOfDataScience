e753a03f27785bab7eb2f679ef9a17ab446d528a
gabir-yusuf <gabir.yusuf@gmail.com>
2019-08-28 20:51:00 +0200

    add Quiz_2.1.sql

---
+++ 2_SQL Joins/Quiz_2.1.sql
@@ -0,0 +1,14 @@
+
+-- @auther: Gabir N. Yousef
+-- @Date: 26 of Aug 2019
+---------------------------------------------------------------------------------
+
+/*
+Quiz 2.1 Description:
+	Provide a table for all web_events associated with account name of Walmart. 	There should be three columns. Be sure to include the primary_poc, time of 		the event, and the channel for each event. Additionally, you might choose to 	add a fourth column to assure only Walmart events were chosen. 
+*/
+SELECT a.primary_poc, w.occurred_at, w.channel, a.name
+FROM web_events w
+JOIN accounts a
+ON w.account_id = a.id
+WHERE a.name = 'Walmart';
