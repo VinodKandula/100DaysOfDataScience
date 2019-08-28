d19bbe580288e7d0d95e4dca6f30019277b1e54b
gabir-yusuf <gabir.yusuf@gmail.com>
2019-08-28 21:15:38 +0200

    add Quiz_3.3.sql

---
+++ 2_SQL Joins/Quiz_3.3.sql
@@ -0,0 +1,15 @@
+-- @auther: Gabir N. Yousef
+-- @Date: 26 of Aug 2019
+---------------------------------------------------------------------------------
+/*
+Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for accounts where the sales rep has a last name starting with K and in the Midwest region. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
+*/
+
+SELECT r.name region, s.name rep, a.name account
+FROM sales_reps s
+JOIN region r
+ON s.region_id = r.id
+JOIN accounts a
+ON a.sales_rep_id = s.id
+WHERE r.name = 'Midwest' AND s.name LIKE '% K%'
+ORDER BY a.name;
