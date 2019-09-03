aa6059054dd4ed591c865c9bf6c38d2922d80467
gabir-yusuf <gabir.yusuf@gmail.com>
2019-09-03 17:32:32 +0200

    add Quiz 3.2

---
+++ 3. SQL Aggregations/Quiz_3.2.sql
@@ -0,0 +1,13 @@
+-- @Auther: Gabir N. Yousef
+-- @Date: 3 of Sep 2019
+---------------------------------------------------------------------------------
+/* Quiz Description:
+
+Find the total sales in usd for each account. You should include two columns - the total sales for each company's orders in usd and the company name.
+*/
+
+SELECT a.name, SUM(total_amt_usd) total_sales
+FROM orders o
+JOIN accounts a
+ON a.id = o.account_id
+GROUP BY a.name;
