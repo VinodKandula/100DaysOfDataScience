9c406b806908e7957dca84815a3061a00d9ed912
gabir-yusuf <gabir.yusuf@gmail.com>
2019-09-03 17:32:22 +0200

    add Quiz 3.1

---
+++ 3. SQL Aggregations/Quiz_3.1.sql
@@ -0,0 +1,16 @@
+-- @Auther: Gabir N. Yousef
+-- @Date: 3 of Sep 2019
+---------------------------------------------------------------------------------
+/* Quiz Description:
+
+Which account (by name) placed the earliest order? Your solution should have the account name and the date of the order.
+*/
+
+SELECT a.name, o.occurred_at
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+ORDER BY occurred_at
+LIMIT 1;
+
+
