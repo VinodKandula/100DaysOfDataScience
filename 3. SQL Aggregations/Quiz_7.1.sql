b8dfa1e1d74f6327af1097e97f7fed9b2c31a371
gabir-yusuf <gabir.yusuf@gmail.com>
2019-09-03 22:04:33 +0200

    Add quiz 7.1

---
+++ 3. SQL Aggregations/Quiz_7.1.sql
@@ -0,0 +1,13 @@
+-- @Auther: Gabir N. Yousef
+-- @Date: 3 of Sep 2019
+---------------------------------------------------------------------------------
+/* Quiz Description:
+Find the sales in terms of total dollars for all orders in each year, ordered from
+greatest to least. Do you notice any trends in the yearly sales totals?
+*/
+
+
+SELECT DATE_PART('year', occurred_at) ord_year,  SUM(total_amt_usd) total_spent
+FROM orders
+GROUP BY 1
+ORDER BY 2 DESC;
