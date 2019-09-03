f87b9066c8026350387a2593055f82e70b434351
gabir-yusuf <gabir.yusuf@gmail.com>
2019-09-03 22:05:23 +0200

    Add quiz 7.4

---
+++ 3. SQL Aggregations/Quiz_7.4.sql
@@ -0,0 +1,13 @@
+-- @Auther: Gabir N. Yousef
+-- @Date: 3 of Sep 2019
+---------------------------------------------------------------------------------
+/* Quiz Description:
+Which month did Parch & Posey have the greatest sales in terms of total number of orders?
+Are all months evenly represented by the dataset?
+*/
+
+SELECT DATE_PART('month', occurred_at) ord_month, COUNT(*) total_sales
+FROM orders
+WHERE occurred_at BETWEEN '2014-01-01' AND '2017-01-01'
+GROUP BY 1
+ORDER BY 2 DESC; 
