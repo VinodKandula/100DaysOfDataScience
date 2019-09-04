4f844d43f5538ec3722f286f7ac4792fc7243e04
gabir-yusuf <gabir.yusuf@gmail.com>
2019-09-04 23:31:46 +0200

    add Quiz 1.2

---
+++ 4. SQL Subqueries & Temporary Tables/Quiz_1.2.sql
@@ -0,0 +1,7 @@
+-- @Auther: Gabir N. Yousef
+-- @Date: 3 of Sep 2019
+---------------------------------------------------------------------------------
+/* Quiz Description:
+
+Who was the primary contact associated with the earliest web_event?
+*/
