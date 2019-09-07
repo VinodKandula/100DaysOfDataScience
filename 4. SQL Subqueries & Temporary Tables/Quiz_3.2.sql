6247ed8791d1c8ec4f40205111bf89a69d07dfda
gabir-yusuf <gabir.yusuf@gmail.com>
2019-09-07 11:56:45 +0200

    add quiz 3.2

---
+++ 4. SQL Subqueries & Temporary Tables/Quiz_3.2.sql
@@ -0,0 +1,27 @@
+-- @Auther: Gabir N. Yousef
+-- @Date: 6 of Sep 2019
+---------------------------------------------------------------------------------
+/* Quiz Description:
+For the region with the largest sales total_amt_usd, how many total orders were placed?
+*/
+
+SELECT r.name, COUNT(o.total) total_orders
+FROM sales_reps s
+JOIN accounts a
+ON a.sales_rep_id = s.id
+JOIN orders o
+ON o.account_id = a.id
+JOIN region r
+ON r.id = s.region_id
+GROUP BY r.name
+HAVING SUM(o.total_amt_usd) = (
+      SELECT MAX(total_amt)
+      FROM (SELECT r.name region_name, SUM(o.total_amt_usd) total_amt
+              FROM sales_reps s
+              JOIN accounts a
+              ON a.sales_rep_id = s.id
+              JOIN orders o
+              ON o.account_id = a.id
+              JOIN region r
+              ON r.id = s.region_id
+              GROUP BY r.name) sub);
