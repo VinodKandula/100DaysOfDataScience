6a19ac0c4eaf9334e573a8967aaca90df3f119a6
gabir-yusuf <gabir.yusuf@gmail.com>
2019-09-07 11:57:02 +0200

    add quiz 3.3

---
+++ 4. SQL Subqueries & Temporary Tables/Quiz_3.3.sql
@@ -0,0 +1,23 @@
+-- @Auther: Gabir N. Yousef
+-- @Date: 6 of Sep 2019
+---------------------------------------------------------------------------------
+/* Quiz Description:
+How many accounts had more total purchases than the account name which has bought
+the most standard_qty paper throughout their lifetime as a customer?
+*/
+
+SELECT COUNT(*)
+FROM (SELECT a.name
+       FROM orders o
+       JOIN accounts a
+       ON a.id = o.account_id
+       GROUP BY 1
+       HAVING SUM(o.total) > (SELECT total
+                   FROM (SELECT a.name act_name, SUM(o.standard_qty) tot_std, SUM(o.total) total
+                         FROM accounts a
+                         JOIN orders o
+                         ON o.account_id = a.id
+                         GROUP BY 1
+                         ORDER BY 2 DESC
+                         LIMIT 1) inner_tab)
+             ) counter_tab;
