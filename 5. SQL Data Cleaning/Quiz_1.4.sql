c751d5b6227ab9400a1473d650b2b04b4b3c235d
gabir-yusuf <gabir.yusuf@gmail.com>
2019-09-07 17:25:57 +0200

    update format

--- 5. SQL Data Cleaning/Quiz_1.4.sql
+++ 5. SQL Data Cleaning/Quiz_1.4.sql
@@ -2,7 +2,8 @@
 -- @Date: 4 of Sep 2019
 ---------------------------------------------------------------------------------
 /* Quiz Description:
-
+Consider vowels as a, e, i, o, and u. What proportion of company names start with a vowel,
+and what percent start with anything else?
 */
 SELECT SUM(vowels) vowels, SUM(other) other
 FROM (SELECT name, CASE WHEN LEFT(UPPER(name), 1) IN ('A','E','I','O','U')
