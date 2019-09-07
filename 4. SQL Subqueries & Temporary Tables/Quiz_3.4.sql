4eb68146d192008341b99d86f2259d1b47399db3
gabir-yusuf <gabir.yusuf@gmail.com>
2019-09-07 11:57:14 +0200

    add quiz 3.4

---
+++ 4. SQL Subqueries & Temporary Tables/Quiz_3.4.sql
@@ -0,0 +1,21 @@
+-- @Auther: Gabir N. Yousef
+-- @Date: 6 of Sep 2019
+---------------------------------------------------------------------------------
+/* Quiz Description:
+For the customer that spent the most (in total over their lifetime as a customer) total_amt_usd,
+ how many web_events did they have for each channel?
+
+*/
+SELECT a.name, w.channel, COUNT(*)
+FROM accounts a
+JOIN web_events w
+ON a.id = w.account_id AND a.id =  (SELECT id
+                     FROM (SELECT a.id, a.name, SUM(o.total_amt_usd) tot_spent
+                           FROM orders o
+                           JOIN accounts a
+                           ON a.id = o.account_id
+                           GROUP BY a.id, a.name
+                           ORDER BY 3 DESC
+                           LIMIT 1) inner_table)
+GROUP BY 1, 2
+ORDER BY 3 DESC;
